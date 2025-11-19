16aa7940e69191f051145e88ef7a938abe5520bb
Marcel Stadelmann <m@marceli.to>
2025-11-19 17:21:01 +0100

    wip

--- resources/views/components/icons/chevron-left.blade.php
+++ resources/views/components/icons/chevron-left.blade.php
@@ -1,3 +1,3 @@
-<svg width="23" height="63" viewBox="0 0 23 63" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ $class ?? '' }}">
-  <path d="M21.8518 61.7503L0.607849 31.0173L21.8518 0.284306" stroke="#9F9F9F"/>
+<svg width="18" height="29" viewBox="0 0 18 29" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ $class ?? '' }}">
+  <path d="M17.345 0.758001L1.536 14.331L17.345 27.904L16.694 28.663L0 14.331L16.694 -1.19209e-07L17.345 0.758001Z" fill="#707070"/>
 </svg>
--- resources/views/components/icons/chevron-right.blade.php
+++ resources/views/components/icons/chevron-right.blade.php
@@ -1,3 +1,3 @@
-<svg width="23" height="63" viewBox="0 0 23 63" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ $class ?? '' }}">
-  <path d="M0.411316 61.7503L21.6553 31.0173L0.411316 0.284306" stroke="#9F9F9F"/>
+<svg width="18" height="29" viewBox="0 0 18 29" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ $class ?? '' }}">
+  <path d="M0 0.758001L15.809 14.331L0 27.904L0.650999 28.663L17.345 14.331L0.650999 -1.19209e-07L0 0.758001Z" fill="#707070"/>
 </svg>
